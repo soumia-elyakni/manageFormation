1d1f8db01a042e0d5b84fd8068851c6fce43836f
soumia elyakni <selyakni@gmail.com>
2023-01-04 09:36:24 +0100

    require dotenv and initData function

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv').config()
+const initData = require('./database/db-init')
 
+
+initData()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,5 +13,4 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`server is running at PORT ${PORT}`);
   });
-
 
