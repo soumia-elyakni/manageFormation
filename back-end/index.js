a183b5c743801f8abb1ab26baa1dc1ed3c270f92
soumia elyakni <selyakni@gmail.com>
2023-01-05 14:38:21 +0100

    require router files

--- back-end/index.js
+++ back-end/index.js
@@ -2,12 +2,18 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 const initData = require('./database/db-init')
+const routes = require('./routers/index')
+
+
 
 
 initData()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api/auth', routes.authRoute)
+// app.use('/api/admin', routes.adminRoute)
+// app.use('/api/emp', routes.employeRoute)
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
