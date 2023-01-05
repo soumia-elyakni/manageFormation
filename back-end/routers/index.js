b453c45dc2533c81e876350550e73c33b9facabc
soumia elyakni <selyakni@gmail.com>
2023-01-05 14:48:30 +0100

    collecte routes

---
+++ back-end/routers/index.js
@@ -0,0 +1,12 @@
+
+const authRoute = require('./authRoutes')
+const adminRoute = require('./adminRoutes')
+const employeRoute = require('./employeRoutes')
+
+
+
+module.exports = {
+    authRoute
+    // adminRoute,
+    // employeRoute
+}
