56616b6f3d3d5ad4721e38846e82347e01bb1efc
soumia elyakni <selyakni@gmail.com>
2023-01-06 16:44:24 +0100

    require formation Controllers

--- back-end/routers/adminRoutes.js
+++ back-end/routers/adminRoutes.js
@@ -2,10 +2,22 @@ const router = require('express').Router()
 const {
     addEmploye
 } = require('../controllers/userControllers')
+
+const {
+    addOrganisme
+} = require('../controllers/organismeControllers')
+
+const {
+    addFormation
+} = require('../controllers/formationControllers')
+
 const {tryCatch} =require('../middlewares/try-catch')
 
 
 router.post('/add-employe', tryCatch(addEmploye))
+router.post('/add-organisme', tryCatch(addOrganisme))
+router.post('/add-formation', tryCatch(addFormation))
+
 
 module.exports = router
 
