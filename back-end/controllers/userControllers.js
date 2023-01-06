1be7fdce39761c2244b982601c2d4c290a0bab06
soumia elyakni <selyakni@gmail.com>
2023-01-06 16:43:41 +0100

    require localStorage and complete addEmploye function

--- back-end/controllers/userControllers.js
+++ back-end/controllers/userControllers.js
@@ -1,10 +1,13 @@
 const Users = require("../models/User");
 const Roles = require("../models/Role");
+const Organismes = require("../models/Organismes");
 
 const bcrypt = require("bcrypt");
+const storage = require("local-storage")
 
 const addEmploye = async (req, res) => {
   const { body } = req;
+
   if (
     !body.email ||
     !body.password ||
@@ -19,12 +22,18 @@ const addEmploye = async (req, res) => {
   const role = await Roles.findOne({ name : "employe"})
   body.role_id = role._id
 
+  const organisme = await Organismes.findOne({ name : body.organisme_id})
+  body.organisme_id = organisme._id
+
   const unhashPassword = body.password;
   const salt = bcrypt.genSaltSync(10);
   body.password = await bcrypt.hash(body.password, salt);
-  console.log({...body, unhashPassword})
 
-
+  const newUser = await Users.create({...body})
+    if(!newUser) throw Error('User not created')
+    storage("unhashPassword", unhashPassword)
+    
+  
 };
 
 module.exports = {  addEmploye }
