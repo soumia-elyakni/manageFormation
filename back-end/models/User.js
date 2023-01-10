0ca9abc12003ad9157756350b980bcab50d1eba6
soumia elyakni <selyakni@gmail.com>
2023-01-10 15:41:14 +0100

    add default proprity to formation_id field in user schema

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -35,7 +35,8 @@ const UserSchema = new mongoose.Schema({
 
     formation_id : {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Formations"
+        ref: "Formations",
+        default : null
     },
 })
 
