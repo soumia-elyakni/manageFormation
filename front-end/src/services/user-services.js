f30a2cbbc58bacc977e43734e5b630bdc4db2bb0
soumia elyakni <selyakni@gmail.com>
2023-01-13 00:34:25 +0100

    create getBoard link

---
+++ front-end/src/services/user-services.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "/api/";
+
+
+const getEmployeBoard = () => {
+  return axios.get(API_URL + "emp", { headers: authHeader() });
+};
+
+
+const getAdminBoard = () => {
+  return axios.get(API_URL + "admin", { headers: authHeader() });
+};
+
+export default {
+  getEmployeBoard,
+  getAdminBoard
+};
