7318b029ed45b4b79789020b5c4c7e813fe294c7
soumia elyakni <selyakni@gmail.com>
2023-01-12 11:11:20 +0100

    create checkifAdmin function and shapping file

--- back-end/middlewares/auth.js
+++ back-end/middlewares/auth.js
@@ -1,18 +1,23 @@
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
+const Roles = require("../models/Role");
 
 const authenticate = (req, res, next) => {
-    if(!req.headers.authorization) throw Error('Missing token')
+  if (!req.headers.authorization) throw Error("Missing token");
 
-    const token = req.headers.authorization.split(' ')[1];
-      try {
-        const user = jwt.verify(token, process.env.TOKEN_CODE);
-        req.user = user;
-        next();
-      } catch (err) {
-        res.send({ message: 'Invalid token' });
-      }
-    
-  };
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const user = jwt.verify(token, process.env.TOKEN_CODE);
+    req.user = user;
+    next();
+  } catch (err) {
+    res.send({ message: "Invalid token" });
+  }
+};
 
-  module.exports = {authenticate}
+const checkAdmin = async (req, res, next) => {
+  const role = await Roles.findOne({ _id: req.user.role_id });
+  if (role.name !== "admin") throw Error("Unauthorized");
+  next();
+};
+
+module.exports = { authenticate, checkAdmin };
