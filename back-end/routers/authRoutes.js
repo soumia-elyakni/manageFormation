e50f2752ec24375bf02ab089395fdd5c715eede6
soumia elyakni <selyakni@gmail.com>
2023-01-12 11:01:10 +0100

    add logout route

--- back-end/routers/authRoutes.js
+++ back-end/routers/authRoutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
+const {authenticate} = require('../middlewares/auth')
 const {
-    Login
+    Login,
+    Logout
 } = require('../controllers/authControllers')
 const {tryCatch} =require('../middlewares/try-catch')
 
 
 router.post('/login', tryCatch(Login))
+router.delete('/logout', authenticate ,tryCatch(Logout))
 
 module.exports = router
