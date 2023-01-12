323e8c76776186ffa07a68f73ad8521dbf741973
soumia elyakni <selyakni@gmail.com>
2023-01-13 00:33:37 +0100

    create authHeader verify

---
+++ front-end/src/services/auth-header.js
@@ -0,0 +1,11 @@
+
+
+const authHeader = (req) => {
+    if (req.headers.authorization) {
+      return { Authorization: 'Bearer ' + req.headers.authorization };
+    } else {
+      return {};
+    }
+  }
+
+export default authHeader
