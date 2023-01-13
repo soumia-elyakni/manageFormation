1ebbeeee4a65e6eecead2ca25cccc9561533a142
soumia elyakni <selyakni@gmail.com>
2023-01-13 17:47:30 +0100

    export actions

--- front-end/src/actions/auth-actions.js
+++ front-end/src/actions/auth-actions.js
@@ -1,19 +1,23 @@
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 
-export function loginSuccess(data) {
+
+const loginSuccess = (data) => {
     return {
       type: LOGIN_SUCCESS,
       payload: { user: data },
     };
   }
   
-  export function loginFailed(error) {
+  const loginFailed = (error) => {
     return {
       type: LOGIN_FAILED,
       payload: { error },
     };
   }
 
-
+  export  {
+    loginSuccess,
+    loginFailed
+  }
 
