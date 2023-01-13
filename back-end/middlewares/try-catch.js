38bf995a5709064e0fd674463b9b4667c61e5b9d
soumia elyakni <selyakni@gmail.com>
2023-01-13 17:44:14 +0100

    add message error

--- back-end/middlewares/try-catch.js
+++ back-end/middlewares/try-catch.js
@@ -2,6 +2,7 @@ const tryCatch = (param) => async(req, res, next) => {
     try {
         await param(req, res)
     } catch (error) {
+        res.status(error.statusCode || 500).json({ error: error.message })
         next(error)
     }
 }
