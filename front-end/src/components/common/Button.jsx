4bb9f051039d0383263fb470f22e10b58fa93325
soumia elyakni <selyakni@gmail.com>
2023-01-13 17:48:03 +0100

    shaping button props

--- front-end/src/components/common/Button.jsx
+++ front-end/src/components/common/Button.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 
-const Button = ({ onClick, value, type, className = "" }) => {
+const Button = props => {
   return (
     <button
-      onClick={onClick}
-      type={type}
+      
+      type={props.type}
       className={
-        `inline-flex items-center px-4 py-2 bg-gray-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest active:bg-gray-900 transition ease-in-out duration-150 
-           'opacity-25'
-           ` + className
+        `inline-flex items-center w-100 h-50 px-4 py-2  border border-transparent rounded-md font-semibold text-xs text-black tracking-widest active:bg-green-900 transition ease-in-out duration-150 
+           'opacity-25' ${props.className} ` 
       }
     >
-      {value}
+      {props.value}
     </button>
   );
 };
