17b0eab410e1a8b93d7f5b99a068f31edfe04b43
soumia elyakni <selyakni@gmail.com>
2023-01-14 11:01:06 +0100

    remove res.sttatus line

--- back-end/middlewares/try-catch.js
+++ back-end/middlewares/try-catch.js
@@ -2,7 +2,6 @@ const tryCatch = (param) => async(req, res, next) => {
     try {
         await param(req, res)
     } catch (error) {
-        res.status(error.statusCode || 500).json({ error: error.message })
         next(error)
     }
 }
