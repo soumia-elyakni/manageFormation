a9e0e54ed4f235e4df765a406ae110d50df73318
soumia elyakni <selyakni@gmail.com>
2023-01-14 11:07:03 +0100

    update className

--- front-end/src/components/common/Button.jsx
+++ front-end/src/components/common/Button.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
+
 const Button = props => {
   return (
     <button
       
       type={props.type}
       className={
-        `inline-flex items-center w-100 h-50 px-4 py-2  border border-transparent rounded-md font-semibold text-xs text-black tracking-widest active:bg-green-900 transition ease-in-out duration-150 
+        `inline-flex items-center w-100 h-50 px-4 py-2  border border-transparent rounded-2 font-semibold text-xs my-2  text-black tracking-widest active:bg-green-900 transition ease-in-out duration-150 
            'opacity-25' ${props.className} ` 
       }
     >
