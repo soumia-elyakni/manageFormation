c2c565942d7ebc1728796ff4a372e18065fea22e
soumia elyakni <selyakni@gmail.com>
2023-01-14 11:04:34 +0100

    replace store and provide

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,20 +2,36 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/pages/auth/Login"
 import { Dashboard } from "./components/pages/admin/Dashboard";
-import {store} from "./store";
-import { Provider } from "react-redux";
+
+import PrivateRoutes from "./components/private/privateRoute";
+import { EmployeBoard } from "./components/pages/employe/EmployeBoard";
 
 
 function App() {
   return (
-    <Provider store={store}>
+    
       <Router>
         <Routes>
+
+      <Route path="" element={<Login />} />
       <Route path="login" element={<Login />} />
-      <Route path="/admin" element={<Dashboard />} />
+
+      
+      <Route element={<PrivateRoutes role="admin" />}>
+      <Route path="/admin" element={<Dashboard />}>
+              
+      </Route>
+      </Route>
+
+      <Route element={<PrivateRoutes role="employe" />}>
+      <Route path="/employe" element={<EmployeBoard />}>
+              
+      </Route>
+      </Route>
+
        </Routes>
       </Router>
-    </Provider>
+    
   );
 }
 
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {store} from "./store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 
