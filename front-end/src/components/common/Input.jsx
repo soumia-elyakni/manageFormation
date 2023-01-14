c5b5af171515062b96b00b23d18446dd91f3a849
soumia elyakni <selyakni@gmail.com>
2023-01-14 11:08:29 +0100

    add placeholde props

--- front-end/src/components/common/Input.jsx
+++ front-end/src/components/common/Input.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
+
 export const Input = props => {
   return (
     <input
       type={props.type}
       value={props.value}
       onChange={props.onChange}
+      placeholder= {props.placeholder}
       name={props.name}
-      className={`w-full px-2 pb-1.5 text-primary outline-none text-base font-light rounded-md ${props.className}`}
+      className={`w-full px-2 pb-3 text-primary outline-none text-base font-light rounded-2 my-2 mx-1 ${props.className}`}
     />
   )
 }
