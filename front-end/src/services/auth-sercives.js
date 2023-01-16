cce418e486f71da8aba4e0f14ee4c17a253b1ab6
soumia elyakni <selyakni@gmail.com>
2023-01-16 14:59:15 +0100

    save login response in localStorage

--- front-end/src/services/auth-sercives.js
+++ front-end/src/services/auth-sercives.js
@@ -1,14 +1,23 @@
 import axios from "axios";
+import authHeader from "./auth-header";
 
 const API_URL = "/api/auth/";
 
-const login = (email, password) => {
-  return axios
+const login = async(email, password) => {
+  const log = await axios
     .post(API_URL + "login", {
       email,
       password,
     })
-
+      localStorage.setItem('user', JSON.stringify(log.data))
+      return log;
 };
 
-export { login };
+const logout = async () => {
+   
+  return await localStorage.removeItem('user')
+  
+}
+
+export { login ,
+         logout};
