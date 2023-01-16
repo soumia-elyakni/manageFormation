d0fd25588a2d57fbea25beb51d49c100d22ae4ad
soumia elyakni <selyakni@gmail.com>
2023-01-16 20:06:35 +0100

    add Onclick props

--- front-end/src/components/common/Button.jsx
+++ front-end/src/components/common/Button.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Button = props => {
   return (
     <button
-      
+      onClick={props.onClick ? props.onClick : null}
       type={props.type}
       className={
         `inline-flex items-center w-100 h-50 px-4 py-2  border border-transparent rounded-2 font-semibold text-xs my-2  text-black tracking-widest active:bg-green-900 transition ease-in-out duration-150 
