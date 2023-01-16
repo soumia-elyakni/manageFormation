2a9dd28b97bc6ffadde9c23ce48167ecf9886584
soumia elyakni <selyakni@gmail.com>
2023-01-16 14:56:39 +0100

    add Organismelist route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,8 @@ import { Dashboard } from "./components/pages/admin/Dashboard";
 import PrivateRoutes from "./components/private/privateRoute";
 import { EmployeBoard } from "./components/pages/employe/EmployeBoard";
 import { AddEmploye } from "./components/tools/AddEmploye";
+import { AddOrganisme } from "./components/tools/AddOrganisme";
+import { Organismeslist } from "./components/pages/admin/Organismeslist";
 
 
 function App() {
@@ -17,12 +19,19 @@ function App() {
       <Route path="" element={<Login />} />
       <Route path="login" element={<Login />} />
 
-      
-      <Route element={<PrivateRoutes role="admin" />}>
       <Route path="/admin" element={<Dashboard />}>
-             <Route path="addEmploye" element={<AddEmploye/>} />
+        <Route path="addEmploye" element={<AddEmploye/>} />
+        <Route path="addOrganisme" element={<AddOrganisme/>} />
+        <Route path="organismes" element={<Organismeslist/>} />
       </Route>
+
+
+      
+      {/* <Route element={<PrivateRoutes role="admin" />}>
+      <Route path="/admin" element={<Dashboard />}>
+             
       </Route>
+      </Route> */}
 
       <Route element={<PrivateRoutes role="employe" />}>
       <Route path="/employe" element={<EmployeBoard />}>
