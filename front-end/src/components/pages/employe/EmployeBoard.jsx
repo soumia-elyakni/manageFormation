948a0271d2a579597123c1d0d7a5b4e67f5df4c8
soumia elyakni <selyakni@gmail.com>
2023-01-16 15:07:05 +0100

    start building employe board

--- front-end/src/components/pages/employe/EmployeBoard.jsx
+++ front-end/src/components/pages/employe/EmployeBoard.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import { useSelector } from 'react-redux/es/exports';
 import { Navigate } from 'react-router-dom'
+import { ActiveUser } from '../../tools/ActiveUser';
+import { AdminSidebar } from '../../tools/AdminSidebar';
+import { Outlet } from 'react-router-dom';
 
 export const EmployeBoard = () => {
-    const { user: currentUser } = useSelector((state) => state.auth);
-    const name = useSelector((state)=> state.auth.user.first_name)
+    // const { user: currentUser } = useSelector((state) => state.auth);
+    // const role = useSelector((state)=> state.auth.user.role)
 
-    if (!currentUser) {
-        return <Navigate to="/login" />;
-      }
+    // if (!currentUser) {
+    //     return <Navigate to="/login" />;
+    //   }
 
 
   return (
-    <div>Hello collab {name}</div>
+    <div className='d-flex flex-row'>
+
+      <div className='d-flex flex-column'>
+
+      <div>
+         <ActiveUser/>
+      </div>
+
+  
+
+      </div>
+
+      <div>
+        <Outlet/>
+      </div>
+      
+    </div>
   )
 }
