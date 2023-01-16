a4bff3d08919e7a7395947f10824e91d20061931
soumia elyakni <selyakni@gmail.com>
2023-01-16 15:05:21 +0100

    build employe card design

---
+++ front-end/src/components/tools/EmployeCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const EmployeCard = () => {
+  return (
+    <div className='Card'>
+      <h4> Name </h4>
+      <h5> organisme name </h5>
+      <h5> current formation </h5> 
+      <button> assigné une autre formation </button>
+    </div>
+  )
+}
