b4d61c1351c52eaffe41f8eaf4ca10b10c5a9758
soumia elyakni <selyakni@gmail.com>
2023-01-16 20:06:00 +0100

    consome organisme data

--- front-end/src/components/tools/OrganismeCard.jsx
+++ front-end/src/components/tools/OrganismeCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-export const OrganismeCard = () => {
+export const OrganismeCard = (props) => {
   return (
 
 <div className="card" style={{width: '18rem'}}>
   <div className="card-body">
-    <h5 className="card-title">Organisme name</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Ville</h6>
-    <p className="card-text"> Address</p>
+    <h5 className="card-title">{props.name}</h5>
+    <h6 className="card-subtitle mb-2 text-muted">{props.ville}</h6>
+    <p className="card-text"> {props.adress}</p>
+    <p>{props.phone}</p>
     <Link to="#" className="card-link">voir les employés</Link>
   </div>
 </div>
