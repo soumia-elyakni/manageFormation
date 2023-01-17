a345a1c68e80680efb317b4709e26614a6fb9bc1
soumia elyakni <selyakni@gmail.com>
2023-01-17 09:18:25 +0100

    Shaping form addOrganisme

--- front-end/src/components/tools/AddOrganisme.jsx
+++ front-end/src/components/tools/AddOrganisme.jsx
@@ -1,50 +1,67 @@
-import React from 'react'
-import { Input } from '../common/Input'
-import Button  from '../common/Button'
-import { useState } from 'react'
-import { addOrganisme } from '../../services/admin-services'
+import React from "react";
+import { Input } from "../common/Input";
+import {Button} from "../common/Button";
+import { useState } from "react";
+import { addOrganisme } from "../../services/admin-services";
 
 export const AddOrganisme = () => {
-   const [organisme, setOrganisme] = useState({
-    name : "",
-    ville : "",
-    adress : "",
-    phone : ""
-   })
+  const [organisme, setOrganisme] = useState({
+    name: "",
+    ville: "",
+    adress: "",
+    phone: "",
+  });
 
   const save = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     addOrganisme(organisme)
-    .then((res) => {
-      console.log(res.data)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-  }
-  
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const handleChange = (e) => {
     e.preventDefault();
-    const value = e.target.value
+    const value = e.target.value;
 
     setOrganisme({
       ...organisme,
-      [e.target.name] :value
-    })
-  }
-  
+      [e.target.name]: value,
+    });
+  };
 
   return (
-    
-      <div className='d-flex fex-column w-100 mx-auto justify-content-center '>   
-        <form className="w-50 d-flex flex-column bg-light  " onSubmit={save}>
-          <Input type="text" name="name" placeholder="name" onChange={handleChange}/>
-          <Input type="text" name="ville" placeholder="ville" onChange={handleChange} />
-          <Input type="text" name="adress" placeholder="address" onChange={handleChange} />
-          <Input type="tel"  name="phone" placeholder="phone" onChange={handleChange} />
-          <Button type="submit" className="bg-success" value="Save"/>
-        </form>
-      </div>
-    
-  )
-}
+    <div className="d-flex fex-column w-100 mx-auto justify-content-center ">
+      <form className="w-50 d-flex flex-column bg-light  " onSubmit={save}>
+        <Input
+          type="text"
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <Input
+          type="text"
+          name="ville"
+          placeholder="ville"
+          onChange={handleChange}
+        />
+        <Input
+          type="text"
+          name="adress"
+          placeholder="address"
+          onChange={handleChange}
+        />
+        <Input
+          type="tel"
+          name="phone"
+          placeholder="phone"
+          onChange={handleChange}
+        />
+        <Button type="submit" className="bg-success" value="Save" />
+      </form>
+    </div>
+  );
+};
