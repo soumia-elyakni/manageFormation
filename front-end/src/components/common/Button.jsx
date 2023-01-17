6c45789258eec555eb10988f1119aa772d19dd62
soumia elyakni <selyakni@gmail.com>
2023-01-17 09:11:16 +0100

    update  Button

--- front-end/src/components/common/Button.jsx
+++ front-end/src/components/common/Button.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-
-const Button = props => {
+const Button = (props) => {
   return (
     <button
       onClick={props.onClick ? props.onClick : null}
       type={props.type}
-      className={
-        `inline-flex items-center w-100 h-50 px-4 py-2  border border-transparent rounded-2 font-semibold text-xs my-2  text-black tracking-widest active:bg-green-900 transition ease-in-out duration-150 
-           'opacity-25' ${props.className} ` 
-      }
+      className={`inline-flex items-center w-100 h-50 px-4 py-2  border border-transparent rounded-2 font-semibold text-xs my-2  text-black tracking-widest active:bg-green-900 transition ease-in-out duration-150 
+           'opacity-25' ${props.className} `}
     >
       {props.value}
     </button>
   );
 };
 
-export default Button
+export  {Button}
