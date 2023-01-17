a6af69a0e9164c3d5a4049b84038ea67dba645a4
soumia elyakni <selyakni@gmail.com>
2023-01-17 09:10:48 +0100

    add usable path

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/pages/auth/Login"
+import Login from "./components/pages/auth/Login";
 import { Dashboard } from "./components/pages/admin/Dashboard";
 
 import PrivateRoutes from "./components/private/privateRoute";
@@ -8,38 +8,35 @@ import { EmployeBoard } from "./components/pages/employe/EmployeBoard";
 import { AddEmploye } from "./components/tools/AddEmploye";
 import { AddOrganisme } from "./components/tools/AddOrganisme";
 import { Organismeslist } from "./components/pages/admin/Organismeslist";
-
+import { EmployeList } from "./components/pages/admin/EmployeList";
+import { FormationList } from "./components/pages/admin/FormationList";
 
 function App() {
   return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="login" element={<Login />} />
 
-  
-      <Routes>
+      <Route path="/admin" element={<Dashboard />}>
+        <Route path="addEmploye" element={<AddEmploye />} />
+        <Route path="addOrganisme" element={<AddOrganisme />} />
+        <Route path="organismes" element={<Organismeslist />} />
+        <Route path="employes" element={<EmployeList />} />
+        <Route path="formations" element={<FormationList/>}/>
+      </Route>
 
-        <Route path="/" element={<Login />} />
-        <Route path="login" element={<Login />} />
-
-        <Route path="/admin" element={<Dashboard />}>
-          <Route path="addEmploye" element={<AddEmploye />} />
-          <Route path="addOrganisme" element={<AddOrganisme />} />
-          <Route path="organismes" element={<Organismeslist />} />
-        </Route>
-
-
-
-              {/* <Route element={<PrivateRoutes role="admin" />}>
+      {/* <Route element={<PrivateRoutes role="admin" />}>
             <Route path="/admin" element={<Dashboard />}>
                   
             </Route>
             </Route> */}
 
-        <Route element={<PrivateRoutes role="employe" />}>
-          <Route path="/employe" element={<EmployeBoard />}> </Route>
+      <Route element={<PrivateRoutes role="employe" />}>
+        <Route path="/employe" element={<EmployeBoard />}>
+          {" "}
         </Route>
-
-      </Routes>
-   
-
+      </Route>
+    </Routes>
   );
 }
 
